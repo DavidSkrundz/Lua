403dda600a70466746f288d5e0630dbcde11889f
David Skrundz <david@skrundz.ca>
2017-06-10 23:07:24 -0600

    Remove Hashable conformance

--- Sources/Lua/Structures/Reference.swift
+++ Sources/Lua/Structures/Reference.swift
@@ -6,15 +6,11 @@
 import CLua
 
 /// Represents a `Lua` reference created by `
-internal struct Reference: RawRepresentable, Hashable, Equatable {
+internal struct Reference: RawRepresentable, Equatable {
 	/// The default `Reference` if it does not exist.  Guaranteed to be unique
 	internal static let None = Reference(rawValue: LUA_NOREF)
 	/// The `Reference` returned for `Nil` objects
 	internal static let Nil  = Reference(rawValue: LUA_REFNIL)
 	
 	internal let rawValue: Index
-	
-	var hashValue: Int {
-		return Int(self.rawValue)
- }
 }
